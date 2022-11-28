668598b890ed44e611e2af9a5518092d031aaacf
Crackita <91308668+Crackita@users.noreply.github.com>
2022-11-28 10:25:53 +0100

    page user

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
+    #[Route('/', name: 'app_user')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
         ]);
     }
 }
