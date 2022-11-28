4b10485aa79725690b72d4d7e27bcb4737f6aeec
Crackita <91308668+Crackita@users.noreply.github.com>
2022-11-28 11:57:23 +0100

    mise en place player

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -11,6 +11,7 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_user')]
     public function index(): Response
     {
+        
         return $this->render('user/home.html.twig', [
             'controller_name' => 'UserController',
         ]);
--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\Player;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<Player>
